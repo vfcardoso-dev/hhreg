b8ce7fa1350a7bea4b480da37f9b743840006e85
Vinicius Cardoso <vinicius.cardoso@sapiensia.com>
2023-04-17 08:43:39 -0300

    Improving math tests

--- hhreg.tests/utilities/SpectreConsoleUtilsTests.cs
+++ hhreg.tests/utilities/SpectreConsoleUtilsTests.cs
@@ -10,38 +10,41 @@ namespace hhreg.tests.utilities;
 
 public class SpectreConsoleUtilsTests : UnitTestsBase
 {
-    [Test]
-    public void deve_calcular_corretamente_linha_de_summary_de_dayEntry()
+    [TestCase(480, 500, "+00:20", null)]
+    [TestCase(480, 400, "-01:20", null)]
+    [TestCase(480, 480, "00:00", "This is a justification")]
+    public void deve_calcular_corretamente_linha_de_summary_de_dayEntry(double workDay, double dayEntryTotalMinutes, string expectedBalance, string justification)
     {
         // Given
         var dayEntry = Fixture.Create<DayEntry>();
-        var workDay = Fixture.Create<double>();
+        dayEntry.TotalMinutes = dayEntryTotalMinutes;
+        dayEntry.Justification = justification;
 
         // When
         var result = SpectreConsoleUtils.GetDayEntrySummaryRow(dayEntry, workDay);
 
         // Then
-        var workDayTs = TimeSpan.FromMinutes(workDay);
         var totalMinutesTs = TimeSpan.FromMinutes(dayEntry.TotalMinutes);
-        var balance = totalMinutesTs.Subtract(workDayTs);
-        var balanceSignal = balance > TimeSpan.Zero ? "+" : "";
         
         ExtractText(result[0]).Should().Be(dayEntry.Day!.ToDateOnly().ToString());
         ExtractText(result[1]).Should().Be(dayEntry.DayType.ToString());
         ExtractText(result[2]).Should().Be(string.Join(" / ", dayEntry.TimeEntries.Select(x => x.Time)));
         ExtractText(result[3]).Should().Be(totalMinutesTs.ToTimeString());
-        ExtractText(result[4]).Should().Be($"{balanceSignal}{balance.ToTimeString()}");
+        ExtractText(result[4]).Should().Be(expectedBalance);
         ExtractText(result[5]).Should().Be(dayEntry.Justification ?? "-");
     }
     
-    [Test]
-    public void deve_calcular_corretamente_linha_de_balance_de_dayEntry()
+    [TestCase(480, 500, 20, "+00:20", "+00:40", null)]
+    [TestCase(480, 400, -20, "-01:20", "-01:40", null)]
+    [TestCase(480, 480, 0, "00:00", "00:00", "This is a justification")]
+    public void deve_calcular_corretamente_linha_de_balance_de_dayEntry(
+        double workDay, double dayEntryTotalMinutes, double initialAcc, string expectedBalance, string expectedAcc, string justification)
     {
         // Given
         var dayEntry = Fixture.Create<DayEntry>();
-        var workDay = Fixture.Create<double>();
-        var startingAccumulation = Fixture.Create<double>();
-        var accumulated = startingAccumulation;
+        dayEntry.TotalMinutes = dayEntryTotalMinutes;
+        dayEntry.Justification = justification;
+        var accumulated = initialAcc;
 
         // When
         var result = SpectreConsoleUtils.GetDayEntryBalanceRow(dayEntry, workDay, ref accumulated);
@@ -50,10 +53,7 @@ public class SpectreConsoleUtilsTests : UnitTestsBase
         var workDayTs = TimeSpan.FromMinutes(workDay);
         var totalMinutesTs = TimeSpan.FromMinutes(dayEntry.TotalMinutes);
         var balance = totalMinutesTs.Subtract(workDayTs);
-        var balanceSignal = balance > TimeSpan.Zero ? "+" : "";
 
-        var accumulatedTs = TimeSpan.FromMinutes(accumulated);
-        var accumulatedSignal = accumulatedTs > TimeSpan.Zero ? "+" : "";
         var timeEntries = dayEntry.TimeEntries.Any() 
             ? string.Join(" / ", dayEntry.TimeEntries.Select(x => x.Time)) 
             : dayEntry.Justification!;
@@ -62,9 +62,9 @@ public class SpectreConsoleUtilsTests : UnitTestsBase
         ExtractText(result[1]).Should().Be(dayEntry.DayType.ToString());
         ExtractText(result[2]).Should().Be(timeEntries);
         ExtractText(result[3]).Should().Be(totalMinutesTs.ToTimeString());
-        ExtractText(result[4]).Should().Be($"{balanceSignal}{balance.ToTimeString()}");
-        ExtractText(result[5]).Should().Be($"{accumulatedSignal}{accumulatedTs.ToTimeString()}");
-        accumulated.Should().Be(startingAccumulation + balance.TotalMinutes);
+        ExtractText(result[4]).Should().Be(expectedBalance);
+        ExtractText(result[5]).Should().Be(expectedAcc);
+        accumulated.Should().Be(initialAcc + balance.TotalMinutes);
     }
 
     private static string ExtractText(IRenderable renderable)
